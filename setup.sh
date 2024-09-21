e9cae7f0374e817f12aa2d3b9fd449fa15983aeb
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2024-09-20 22:42:14 -0400

    no set -u

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -u
-
 # Install Nix
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
   sh -s -- install --no-confirm --extra-conf "trusted-users = $(whoami)"
