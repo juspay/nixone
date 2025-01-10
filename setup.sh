82f1ae1f6db21a4601dfbaabc09fab0f7bdaf8be
shivaraj-bh <me@shivaraj-bh.in>
2025-01-11 02:42:26 +0530

    Use `--extra-experimental-features` everywhere; Use `om.templates.darwin`

--- setup.sh
+++ setup.sh
@@ -9,21 +9,22 @@ curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix 
 
 # Run `om health`
 echo "\n# Check nix health"
-nix --accept-flake-config run github:juspay/omnix health
+nix --accept-flake-config --extra-experimental-features "nix-command flakes" run github:juspay/omnix health
 
-# Setup nix-dev-home
-echo "\n# Setting up home-manager & direnv"
-mkdir -p ~/nixconfig && cd ~/nixconfig
-nix --accept-flake-config run github:juspay/omnix -- \
-  init github:juspay/nixos-unified-template#nix-darwin -o . \
+# Setup nixos-unified-template
+echo "\n# Setting up nix-darwin & direnv"
+nix --accept-flake-config --extra-experimental-features "nix-command flakes" run github:juspay/omnix -- \
+  init github:juspay/nixos-unified-template#darwin -o ~/nixconfig \
   --non-interactive \
   --params '{"username":"'$(id -un)'", "git-name":"'$(id -F)'", "git-email":"'$(id -un)'@juspay.in", "hostname": "'$(hostname -s)'"}'
 
 # Avoid any pre-defined nix.conf
+echo "\n# Removing /etc/nix/nix.conf"
 sudo rm /etc/nix/nix.conf
 
-nix --extra-experimental-features "nix-command flakes" run
+cd ~/nixconfig && nix --extra-experimental-features "nix-command flakes" run
 
 echo "\n# All done 🥳 Please start a **new terminal window**"
 # TODO: Can we automate this? This doesn't work
 # env -i HOME="$HOME" "$SHELL" -l
+
